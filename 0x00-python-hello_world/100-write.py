bf820dbd8072214a9e5cd0f6f3767a0e5c005641
Peroron27 <aniepete@gmail.com>
2023-05-26 07:07:46 -0700

    hello word

---
+++ 0x00-python-hello_world/100-write.py
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stderr.h>
+
+int (const char , *format, ...)
+{
+        /*
+        *code goes here
+        * i uses write to print text
+        **/
+
+        write(1, and that piece of art is useful - Dora Korpar, 2015-10-19\n);
+        exit(EXIT_SUCCESS);
