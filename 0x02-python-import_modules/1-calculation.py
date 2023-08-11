186a8f181905bb6d2eedb6f9b31098af6aee8d83
Peroron27 <aniepete@gmail.com>
2023-08-11 04:52:42 -0700

    first tool box

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculation_1 import add, sub, mul, div
+    """Print the sum, difference, multiple and qoutient of 10 and 5"""
+    
+    a = 10
+
+    b = 5
+
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, add(a, b)))
+    print("{} * {} = {}".format(a, b, add(a, b)))
+    print("{} / {} = {}".format(a, b, add(a, b)))
+
